f1caf1668251021ab9fecc94e834a8ab3de3dbe4
begonia <begonia@hackbright.com>
2016-07-26 21:17:47 -0700

    Initial Commit

---
+++ calculator.py
@@ -0,0 +1,42 @@
+def addition(num1, num2):
+	return num1 + num2
+
+def subtraction(num1, num2):
+	return num2 - num1
+
+def multiplication(num1, num2):
+	return num1 * num2
+
+def division(num1, num2):
+	return num1 / num2
+
+def modulo(num1, num2):
+	return num1 % num2
+
+def power(base, exponent):
+	return base ** exponent
+
+def square(num1):
+	return num1 * num1	
+
+nine = addition(4, 5)
+three = subtraction(9, 6)
+
+# print addition(nine, three)
+
+part1 = division(12, 2)
+
+# print subtraction(60, part1)
+
+# print addition(1, 2) + 3
+
+print addition(1, 2) ** 2
+
+
+# print addition(4, 5)
+# print subtraction(10, 5)
+# print multiplication(5, 10)
+# print division(10, 5)
+# print modulo(10, 9)
+# print power(2, 3)
+# print square(4)
